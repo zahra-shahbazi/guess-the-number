b01d2aac30855caffe98727af31afc3ffd7f17ee
zahra shahbazi <zahra.shahbazi.nafas@gmail.com>
2025-11-30 20:04:06 +0330

    guess the number

---
+++ game.py
@@ -0,0 +1,16 @@
+firstnumber=input('chose a nuber 1-9')
+
+firstnumber=int(firstnumber)
+
+number=firstnumber
+
+number *=2
+number +=8
+number +=firstnumber
+number -=2
+number //=3
+number -=firstnumber
+number *=4
+
+
+print(number)
